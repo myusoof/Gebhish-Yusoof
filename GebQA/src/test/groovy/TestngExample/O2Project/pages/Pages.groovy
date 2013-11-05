7350d6f40df0c4631ace71e6fc7ca7eddd3a692f
mohmedy <mohmedyusoof.as@cognizant.com>
2013-11-05 16:48:05 +0000

    Fourth commit

---
+++ GebQA/src/test/groovy/TestngExample/O2Project/pages/Pages.groovy
@@ -0,0 +1,46 @@
+package TestngExample.O2Project.pages
+
+import TestngExample.O2Project.modules.GoogleSearchModule
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ee
+ * Date: 05/11/13
+ * Time: 15:52
+ * To change this template use File | Settings | File Templates.
+ */
+import geb.Browser
+import geb.Page
+import geb.Module
+
+// modules are reusable fragments that can be used across pages that can be paramaterised
+// here we are using a module to model the search function on the home and results pages
+
+class GoogleHomePage extends Page {
+
+    // pages can define their location, either absolutely or relative to a base
+    static url = "http://google.com/ncr"
+
+    // “at checkers” allow verifying that the browser is at the expected page
+    static at = { title == "Google" }
+
+    static content = {
+        // include the previously defined module
+        search { module GoogleSearchModule, buttonValue: "Google Search" }
+    }
+}
+
+class GoogleResultsPage extends Page {
+    static at = { title.endsWith "Google Search" }
+    static content = {
+        // reuse our previously defined module
+        search { module GoogleSearchModule, buttonValue: "Search" }
+
+        // content definitions can compose and build from other definitions
+        results { $("li.g") }
+        result { i -> results[i] }
+        resultLink { i -> result(i).find("a.l") }
+        firstResultLink { resultLink(0) }
+    }
+}
+
