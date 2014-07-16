36a0e195feddaed926d61df438d8e7e7fa5a9f13
mohmedy <mohmedyusoof.as@cognizant.com>
2014-07-16 13:16:14 +0100

    Http changes

--- HTTPClientModule/src/main/groovy/HttpExampleTutorial/LowLevelResource.groovy
+++ HTTPClientModule/src/main/groovy/HttpExampleTutorial/LowLevelResource.groovy
@@ -7,9 +7,11 @@ import org.apache.http.HttpEntity
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.BufferedHttpEntity
+import org.apache.http.entity.FileEntity
 import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
+import org.apache.http.entity.ContentType
 
 /**
  * Created with IntelliJ IDEA.
@@ -28,11 +30,14 @@ class LowLevelResource {
         entity = new BufferedHttpEntity(entity)
 //        client.execute(get).getEntity().writeTo(new FileOutputStream("/home/yusoof/projects/Gebhish-Yusoof/HTTPClientModule/src/main/resources/TestOne.json"))
         String outputResponse = EntityUtils.toString(entity)
-
+        println JSON.parse(outputResponse).results[0].address_components[0].long_name
 //        println JSON.parse(outputResponse).results[0].address_components[0].long_name
-
-        HttpEntity entity1 = client.execute(new HttpGet("http://localhost:8350/service/user")).entity
-        client.execute(new HttpPost("http://localhost:8350/service/login")).entity
-        println EntityUtils.toString(entity1)
+//
+//        HttpEntity entity1 = client.execute(new HttpGet("http://localhost:8350/service/user")).entity
+//        HttpPost post = new HttpPost("http://localhost:8350/service/login")
+//        FileEntity fileEntity = new FileEntity(new File("/home/ee/projects/Gebhish-Yusoof/HTTPClientModule/src/main/resources/userdetails.json"), ContentType.APPLICATION_JSON)
+//                post.setEntity(fileEntity)
+//        entity1 = client.execute(post).entity
+//        println EntityUtils.toString(entity1)
     }
 }
