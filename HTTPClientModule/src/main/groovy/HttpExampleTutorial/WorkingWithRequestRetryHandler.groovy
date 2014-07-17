1295518f69c3c391e314ab4f8f4f72bef33f78ae
mohmedy <mohmedyusoof.as@cognizant.com>
2014-07-17 13:51:55 +0100

    Http interceptor

--- HTTPClientModule/src/main/groovy/HttpExampleTutorial/WorkingWithRequestRetryHandler.groovy
+++ HTTPClientModule/src/main/groovy/HttpExampleTutorial/WorkingWithRequestRetryHandler.groovy
@@ -1,19 +1,25 @@
 package HttpExampleTutorial
 
+import org.apache.http.HttpEntity
 import org.apache.http.HttpEntityEnclosingRequest
+import org.apache.http.HttpException
 import org.apache.http.HttpRequest
+import org.apache.http.HttpRequestInterceptor
 import org.apache.http.HttpResponse
 import org.apache.http.client.ClientProtocolException
 import org.apache.http.client.HttpRequestRetryHandler
 import org.apache.http.client.ResponseHandler
 import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.methods.HttpPost
 import org.apache.http.client.protocol.HttpClientContext
 import org.apache.http.conn.ConnectTimeoutException
 import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.protocol.HttpContext
+import org.apache.http.protocol.HttpRequestInterceptorList
 
 import javax.net.ssl.SSLException
+import java.util.concurrent.atomic.AtomicInteger
 
 
 /**
@@ -28,7 +34,7 @@ class WorkingWithRequestRetryHandler {
 
         HttpRequestRetryHandler retryHandler = new HttpRequestRetryHandler() {
             @Override
-            public boolean retryRequest(IOException exception,int executionCount,HttpContext context) {
+            public boolean retryRequest(IOException exception, int executionCount, HttpContext context) {
                 if (executionCount >= 5) {
                     return false;
                 }
@@ -47,14 +53,22 @@ class WorkingWithRequestRetryHandler {
                 HttpClientContext clientContext = HttpClientContext.adapt(context)
                 HttpRequest request = clientContext.getRequest()
                 boolean idempotent = !(request instanceof HttpEntityEnclosingRequest)
-                if(idempotent){
+                if (idempotent) {
                     return true
                 }
                 return false
             }
         }
 
-        CloseableHttpClient client = HttpClients.custom().setRetryHandler(retryHandler).build()
+        HttpRequestInterceptor httpRequestInterceptor = new HttpRequestInterceptor() {
+            @Override
+            void process(HttpRequest request, HttpContext context) throws HttpException, IOException {
+                AtomicInteger count = (AtomicInteger) context.getAttribute("count")
+                request.addHeader("count", Integer.toString(count.getAndIncrement()))
+            }
+        }
+
+        CloseableHttpClient client = HttpClients.custom().setRetryHandler(retryHandler).addInterceptorLast(httpRequestInterceptor).build()
 
         ResponseHandler<String> hr = new ResponseHandler<String>() {
             @Override
@@ -62,8 +76,16 @@ class WorkingWithRequestRetryHandler {
                 return "Yusoof"
             }
         }
+        AtomicInteger count = new AtomicInteger(0)
+        HttpClientContext clientContext = HttpClientContext.create()
+        clientContext.setAttribute("count", count)
+
         HttpGet get = new HttpGet("http://the-internet.herokuapp.com/")
-        String a = client.execute(get, hr)
-        println a
+        for (int i = 0; i <= 10; i++) {
+            HttpResponse response= client.execute(get, clientContext)
+            HttpEntity entity = response.entity
+            println clientContext.getAttribute("count")
+            response.close()
+        }
     }
 }
