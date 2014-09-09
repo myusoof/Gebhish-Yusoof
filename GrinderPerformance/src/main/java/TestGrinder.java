5ec3580d142e95c50377dc7cefb976b8ff4be30f
mohmedy <mohmedyusoof.as@cognizant.com>
2014-09-09 19:30:17 +0100

    Updated the working grinder properties

---
+++ GrinderPerformance/src/main/java/TestGrinder.java
@@ -0,0 +1,14 @@
+import net.grinder.Grinder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ee
+ * Date: 09/09/14
+ * Time: 19:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestGrinder {
+    public static void main(String[] args) {
+
+    }
+}
